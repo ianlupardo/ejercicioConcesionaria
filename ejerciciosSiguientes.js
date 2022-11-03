4d947bdbcab6c4e79fc133b570e080d89edbee78
ianlupardo <ianlupardo@gmail.com>
2022-11-02 22:40:44 -0300

    Actividad finalizada

--- ejerciciosSiguientes.js
+++ ejerciciosSiguientes.js
@@ -1,32 +1,69 @@
-{const autos = require("./autos")
+const autos = require('./autos');
 
 const concesionaria = {
-    autos: autos
-    buscarAuto : function(patente){
-        let i = autos.findIndex (x => x.patente == patente);
-      
-        return i != -1 ? autos[i] : null;
-     },
-    
-     venderAuto: function (patente) {
-        let auto = this.buscarAuto(patente);
-        if (auto) {
-          auto.vendido = true;
-         }
-},
+   autos: autos,
+   buscarAuto : function (patente) {
+       let auto = null;
+       for (let i = 0; i < autos.length; i++) {
+           if (autos[i].patente == patente) {
+               auto = autos[i];
+           }
+       }
+       return auto;
+   },
+   
+   venderAuto : function (patente) {
+       let auto = this.buscarAuto(patente);
+       if (auto != null) {
+           let indice = concesionaria.autos.indexOf(auto);
+           autos[indice].vendido = true;
+       }
+   },
 
-autosParaLaVenta: function(){
-    return autos = this.autos.filter(function (autos){
-          return !autos.vendido
-       })
-    },
-autosNuevos : function(autos){
-    const auto = this.autosParaLaVenta(function(autos){
-   if (auto.km<100){
-            return auto
-        }
-    })
-}
-};
+   autosParaLaVenta : function () {
+       let autosParaLaVenta = this.autos.filter(function (auto) { return !auto.vendido});
+       return autosParaLaVenta;
+   },
 
-}
+   autosNuevos : function () {
+       let autosParaLaVenta = this.autosParaLaVenta();
+       let autosNuevos = autosParaLaVenta.filter(auto => auto.km < 100);
+       return autosNuevos;
+   },
+
+   listaDeVentas: function () {
+       listado = []; 
+       this.autos.forEach(function(auto){
+           if (auto.vendido == true) {
+               listado.push(auto.precio);
+           }
+       }) 
+       return listado;
+   },
+
+   totalDeVentas: function () {
+       let listaVentas = this.listaDeVentas();
+       let total = 0;
+       if (listaVentas.length != 0) {
+          total = listaVentas.reduce((acum, val) => acum + val);
+       }
+       return total;
+   },
+
+   puedeComprar: function (auto,persona){
+       let autos = this.autosParaLaVenta();
+       let indice = autos.indexOf(auto);
+       if (indice != -1) { 
+       let precio = autos[indice].precio;
+       let cuotas = autos[indice].cuotas;
+       return precio <= persona.capacidadDePagoTotal && (precio / cuotas) <= persona.capacidadDePagoEnCuotas;
+       } else { return "El auto puede ser adquirido"};
+   },
+
+autosQuePuedeComprar: function (cliente) {
+       let autos = this.autosParaLaVenta();
+       let compra = autos.filter((auto) => this.puedeComprar(auto,cliente));
+       return compra;
+   },
+
+   };
